fccfd8aa31ff0d7706e22f54a7243398dd5dca7b
Julien Leal <julienbreno.nave@gmail.com>
2015-12-23 15:47:48 -0200

    resolvido

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT||3000;
+
+var middleware = require('./middleware');
+
+
+ app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication, function (req, res){
+	res.send('its about us page');
+});
 
 app.use(express.static(__dirname+'/public'));
 
