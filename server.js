f6dc5cac4a26ae3f4d3701cea779a98a10d9baf3
Julien Leal <julienbreno.nave@gmail.com>
2015-12-23 15:44:47 -0200

    teste

--- server.js
+++ server.js
@@ -2,15 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.port || 3000;
 
-var middleware = require('./middleware');
-
-
- app.use(middleware.logger);
-
-app.get('/about',middleware.requireAuthentication, function (req, res){
-	res.send('its about us page');
-});
-
 app.use(express.static(__dirname+'/public'));
 
 app.listen(port, function(){
